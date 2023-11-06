69e42f8d0948e1b89a19a5393bc8b83c9847f9ce
ConnorBP <connor.postma@gmail.com>
2023-11-06 18:24:34 -0500

    fixed deployment batch script file paths for windows

--- deploy.bat
+++ deploy.bat
@@ -2,9 +2,9 @@ ECHO BUILDING PROFILE RELEASE
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/wasm_battle_arena.wasm
 ECHO EXTRA OPTIMIZE
-rem mkdir tmp
-rem copy out/wasm_battle_arena_bg.wasm tmp/wasm_battle_arena_tmp.wasm
-rem wasm-opt -O -ol 100 -s 100 -o ./out/wasm_battle_arena_bg.wasm ./tmp/wasm_battle_arena_tmp.wasm
+mkdir tmp
+copy /b ".\\out\\wasm_battle_arena_bg.wasm" ".\\tmp\\wasm_battle_arena_tmp.wasm"
+wasm-opt -O -ol 100 -s 100 -o ./out/wasm_battle_arena_bg.wasm ./tmp/wasm_battle_arena_tmp.wasm
 
 rem ECHO CLEANUP
-rem del tmp/wasm_battle_arena_tmp.wasm
+del ".\\tmp\\wasm_battle_arena_tmp.wasm"
